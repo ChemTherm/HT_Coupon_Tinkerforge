9f0fe87febaa64540058dd5a10cf633fe217bd1b
Christian_PC <christian.walter@chem-therm.de>
2023-03-29 15:53:00 +0200

    updatet GUI from develop

--- icvt_HT_Coupon.py
+++ icvt_HT_Coupon.py
@@ -2,24 +2,32 @@ import tkinter as tk
 import time
 import argparse
 import json
-import customtkinter
+import customtkinter as ctk
 from PIL import Image,ImageTk
-from datetime import datetime
-from source.icvt.tinkerforge_lib import *
+from datetime import datetime, timedelta
+from ChemTherm_library.tinkerforge_lib import *
 
 
 
 def tk_loop():
 
-    for tc_obj_name in tc_list:
-        lable_t_ist_dict[tc_obj_name].config(text = str(tc_list[tc_obj_name].t)+"°C")
+
+    if running == 1:
+        T_set = json_timing(config,section,t0)
+        for i in lable_T_soll:
+            lable_T_soll[i].delete(0, tk.END)
+            lable_T_soll[i].insert(0,str(T_set[i]))
+            
+    for i in lable_T_ist:
+        progressbar[i].set(patronen_list[p_obj_name].pwroutput/100)
+        lable_T_ist[i].config(text = str(tc_list[i].t)+" °C")
     
     MFC_N2.get()
-    value_MFC_N2.config(text = str(MFC_N2.Voltage) + " mV") 
     MFC_Air.get()
     MFC_Ethan.get()
-    value_MFC_Air.config(text = str(MFC_Air.Voltage) + " mV") 
-    value_MFC_Ethan.config(text = str(MFC_Ethan.Voltage) + " mV") 
+    value_MFC[0].config(text = str(MFC_N2.Voltage) + " mV") 
+    value_MFC[1].config(text = str(MFC_Air.Voltage) + " mV") 
+    value_MFC[2].config(text = str(MFC_Ethan.Voltage) + " mV") 
     pressure1.get()
     pressure2.get()
     #pressure3.get()
@@ -34,9 +42,9 @@ def tk_loop():
             line += str(tc_list[tc_obj_name].t) + '\t'
         for p_obj_name in patronen_list:
             line += str(patronen_list[p_obj_name].pwroutput) + '\t'
-        line += str(set_MFC_N2.get()) + ' \t '+ str(MFC_N2.Voltage) + ' \t '
-        line += str(set_MFC_Air.get()) + ' \t '+ str(MFC_Air.Voltage) + ' \t '
-        line += str(set_MFC_Ethan.get()) + ' \t '+ str(MFC_Ethan.Voltage) + ' \t '
+        line += str(set_MFC[0].get()) + ' \t '+ str(MFC_N2.Voltage) + ' \t '
+        line += str(set_MFC[1].get()) + ' \t '+ str(MFC_Air.Voltage) + ' \t '
+        line += str(set_MFC[2].get()) + ' \t '+ str(MFC_Ethan.Voltage) + ' \t '
         line += str(pressure1.Voltage) + ' \t '
         #line += ' /n'
         f.writelines(line)
@@ -53,40 +61,52 @@ def tk_loop():
     window.after(500, tk_loop)
 
 def getdata():    
-    for i in set_temp_dict:
-        t_soll[i] = set_temp_dict[i].get()
-        print(t_soll[i])
 
-    if set_temp_dict['T1'].get() != '':
-        patrone_1.set_t_soll(float(set_temp_dict['T1'].get()))
-    if set_temp_dict['T2'].get() != '':
-        patrone_2.set_t_soll(float(set_temp_dict['T2'].get()))
-    if set_temp_dict['T3'].get() != '':
-        patrone_3.set_t_soll(float(set_temp_dict['T3'].get()))
-    if set_temp_dict['T4'].get() != '':
-        patrone_4.set_t_soll(float(set_temp_dict['T4'].get()))
-    if set_temp_dict['T5'].get() != '':
-        patrone_5.set_t_soll(float(set_temp_dict['T5'].get()))
-    if set_temp_dict['T6'].get() != '':
-        patrone_6.set_t_soll(float(set_temp_dict['T6'].get()))
-    if set_temp_dict['T7'].get() != '':
-        patrone_7.set_t_soll(float(set_temp_dict['T7'].get()))
-    if set_temp_dict['T8'].get() != '':
-        patrone_8.set_t_soll(float(set_temp_dict['T8'].get()))
+    if lable_T_soll[0].get() != '':
+        patrone_1.set_t_soll(float(lable_T_soll[0].get()))
+    if lable_T_soll[1].get() != '':
+        patrone_2.set_t_soll(float(lable_T_soll[1].get()))
+    if lable_T_soll[2].get() != '':
+        patrone_3.set_t_soll(float(lable_T_soll[2].get()))
+    if lable_T_soll[3].get() != '':
+        patrone_4.set_t_soll(float(lable_T_soll[3].get()))
+    if lable_T_soll[4].get() != '':
+        patrone_5.set_t_soll(float(lable_T_soll[4].get()))
+    if lable_T_soll[5].get() != '':
+        patrone_6.set_t_soll(float(lable_T_soll[5].get()))
+    if lable_T_soll[6].get() != '':
+        patrone_7.set_t_soll(float(lable_T_soll[6].get()))
+    if lable_T_soll[7].get() != '':
+        patrone_8.set_t_soll(float(lable_T_soll[7].get()))
 
 
-    MFC_N2_soll = int(set_MFC_N2.get())
-    MFC_N2.set(MFC_N2_soll)
+    MFC_N2.set(int(set_MFC[0].get()))
+    MFC_Air.set(int(set_MFC[1].get()))
+    MFC_Ethan.set(int(set_MFC[2].get()))
 
-    MFC_Air_soll = int(set_MFC_Air.get())
-    MFC_Air.set(MFC_Air_soll)
+def Start_Button_callback():
+    global section, running, t0
+    running = 1
+    section = 1
+    t0 = time.time()
+    Start_Button.configure(state = "disabled")
 
-    MFC_Ethan_soll = int(set_MFC_Ethan.get())
-    MFC_Ethan.set(MFC_Ethan_soll)
+def Stop_Button_callback():
+    global section, running, t0
+    running = 0
+    Start_Button.configure(state = "enabled")
 
+'''' 
+====================================
+SETUP 
+====================================
+'''
+t0 = time.time()
+section = 0
+running = 0
 
-filename = "20230324_Coking_lowResidence_28_23.dat"
-#filename = "Test.dat"
+#filename = "20230324_Coking_lowResidence_28_23.dat"
+filename = "Test.dat"
 
 with open(filename, 'a') as f:
     headline = "time \t t1 \t t2 \t t3 \t t4 \t t5 \t t6 \t t7 \t t8  \t p1 \t p2 \t p3 \t p4 \t p5 \t p6 \t p7 \t p8  \t MFC_N2_soll \t MFC_N2_ist \t MFC_Air_soll \t MFC_Air_ist \t MFC_Ethan_soll \t MFC_Ethan_ist \t Druck1 \n"
@@ -110,38 +130,42 @@ if json_name == "None":
 
 '''
 ====================================
-LOAD JSON FILE
+LOAD JSON FILEs
 ====================================
 '''
 with open(json_name +'.json', 'r') as config_file:
     config = json.load(config_file)
 
-#window = set_ICVT_landingpage(config)
-window = tk.Tk()
+with open('img/setting' + str(config['REACTOR']['amount'])  + '.json', 'r') as img_file:
+    img = json.load(img_file)
+
+window = ctk.CTk()
+ctk.set_appearance_mode("light")
 scrW = window.winfo_screenwidth()
 scrH = window.winfo_screenheight()
 window.geometry(str(scrW) + "x" + str(scrH))
 window.title(config['REACTOR']['Name'])
 window.attributes('-fullscreen',True)
 
-
-
 #----------- Images ----------- 
-bg_image = ImageTk.PhotoImage(Image.open(config['PATH']['images'] + 'reactor_background.png').resize((scrW,scrH),Image.LANCZOS))
-close_img = ImageTk.PhotoImage(Image.open(config['PATH']['images'] + 'close.png').resize((50,50),Image.LANCZOS))
+bg_image = ImageTk.PhotoImage(Image.open(img['Background']['name']).resize((int(img['Background']['width']*img['Background']['factor']),int(img['Background']['height']*img['Background']['factor'])),Image.LANCZOS))
+close_img = ctk.CTkImage(Image.open(config['PATH']['images'] + 'close.png'),size=(80, 80))
+bg_image = ctk.CTkImage(Image.open(img['Background']['name']),size=(int(img['Background']['width']), int(img['Background']['height'])))
+
+
 
 #----------- Frames ----------
-
-frame_MFC = tk.Frame(window, bg=config['TKINTER']['background-color'], bd=0, height=scrH, width=scrW)
-
+lf_MFC = ctk.CTkFrame(window, border_color=config['TKINTER']['background-color'], border_width=0, height=scrH, width=scrW)
+name_Frame = ctk.CTkLabel(lf_MFC, font = ('Arial',16), text='MFC Steuerung')
+name_Frame.grid(column=0, columnspan = 2, row=0, ipadx=5, ipady=5)
+lf_MFC.place(x= 50,y= 800)
 
 #----------- Labels -----------
-#frame_bg = tk.Label(master=window,image=bg_image)
-
-
 #canvas_bg.pack_forget()
-label_background = tk.Label(window,image=bg_image)
-label_background.place(x=0,y=0)
+label_background = ctk.CTkLabel(window,image=bg_image,text="")
+x_offset = img['Background']['x']
+y_offset = img['Background']['y']
+label_background.place(x = x_offset,y = y_offset)
 label_background.lower()
 
 
@@ -169,43 +193,48 @@ value_pressure3.grid(column=1, row=2, ipadx=5, ipady=5)
 lf_control = tk.LabelFrame(window, text='Steuerung')
 lf_control.grid(column=0, row=1, padx=20, pady=20)
 
-frame_MFC.place(x= 1050,y= 100)
-lf_MFC = tk.LabelFrame(frame_MFC, text='MFC Steuerung')
-lf_MFC.grid(column=0, row=0, padx=5, pady=5)
-Lable_MFC_N2= tk.Label(lf_MFC, text='MFC_N2 ')
-Lable_MFC_N2.grid(column=0, row=0, ipadx=5, ipady=5)
-set_MFC_N2 = tk.Entry(lf_MFC, width= 10)
-set_MFC_N2.grid(column=1, row=0, ipadx=5, ipady=5)
-unit_MFC_N2= tk.Label(lf_MFC, text=' mV')
-unit_MFC_N2.grid(column=2, row=0, ipadx=5, ipady=5)
-value_MFC_N2= tk.Label(lf_MFC, text='NaN mV')
-value_MFC_N2.grid(column=3, row=0, ipadx=5, ipady=5)
-# MFC Air
-Lable_MFC_Air= tk.Label(lf_MFC, text='MFC_Air ')
-Lable_MFC_Air.grid(column=0, row=1, ipadx=5, ipady=5)
-set_MFC_Air = tk.Entry(lf_MFC, width= 10)
-set_MFC_Air.grid(column=1, row=1, ipadx=5, ipady=5)
-unit_MFC_Air= tk.Label(lf_MFC, text=' mV')
-unit_MFC_Air.grid(column=2, row=1, ipadx=5, ipady=5)
-value_MFC_Air= tk.Label(lf_MFC, text='NaN mV')
-value_MFC_Air.grid(column=3, row=1, ipadx=5, ipady=5)
-# MFC Ethan
-Lable_MFC_Ethan= tk.Label(lf_MFC, text='MFC_Ethan ')
-Lable_MFC_Ethan.grid(column=0, row=2, ipadx=5, ipady=5)
-set_MFC_Ethan = tk.Entry(lf_MFC, width= 10)
-set_MFC_Ethan.grid(column=1, row=2, ipadx=5, ipady=5)
-unit_MFC_Ethan= tk.Label(lf_MFC, text=' mV')
-unit_MFC_Ethan.grid(column=2, row=2, ipadx=5, ipady=5)
-value_MFC_Ethan= tk.Label(lf_MFC, text='NaN mV')
-value_MFC_Ethan.grid(column=3, row=2, ipadx=5, ipady=5)
+lable_T_ist ={}
+progressbar ={}
+for i in range(0,8):
+    lable_T_ist[i] = ctk.CTkLabel(window, font = ('Arial',16), text='0 °C')
+    lable_T_ist[i].place(x = x_offset + img['T-Reaktor']['x'][i],y = y_offset + img['T-Reaktor']['y'][i])
 
+    progressbar[i] = ctk.CTkProgressBar(master=window, width = 80, progress_color = 'red')
+    progressbar[i] .place(x = x_offset + img['T-Reaktor']['x'][i]-8,y = y_offset + img['T-Reaktor']['y'][i]+30)
 
+lable_T_soll ={}
+for i in range(0,4):
+    lable_T_soll[i] = tk.Entry(window, font = ('Arial',16), width = 6,bg='light blue' )
+    lable_T_soll[i].place(x = x_offset + img['T-Set']['x'][i],y = y_offset + img['T-Set']['y'][i])
+
+name_MFC={}; set_MFC={}; unit_MFC={}; value_MFC={}
+for i in range(0,3):
+    name_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text=config['MFC']['name'][i])
+    name_MFC[i].grid(column=0, row=i+1, ipadx=5, ipady=5)
+    set_MFC[i] = tk.Entry(lf_MFC, font = ('Arial',16), width = 6 )
+    set_MFC[i].grid(column=1, row=i+1, ipadx=5, ipady=5)
+    unit_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text=' mV')
+    unit_MFC[i].grid(column=2, row=i+1, ipadx=5, ipady=5)
+    value_MFC[i]= ctk.CTkLabel(lf_MFC, font = ('Arial',16), text='0 mV')
+    value_MFC[i].grid(column=3, row=i+1, ipadx=5, ipady=5)
+
+#----------- Buttons -----------
 button1 = tk.Button(lf_control,text='set values', command=getdata, bg='brown', fg='white')
 button1.grid(column=3, row=9, ipadx=10, ipady=10)
+Start_Button = ctk.CTkButton(master=window, command=Start_Button_callback,text="Messung starten", font = ('Arial',16))
+Start_Button.place(x=300, y=50)
+Stop_Button = ctk.CTkButton(master=window, command=Stop_Button_callback,text="Messung stoppen", font = ('Arial',16))
+Stop_Button.place(x=300, y=100)
+Exit_Button = ctk.CTkButton(master=window,text="", command=window.destroy, fg_color= 'transparent',  hover_color='#F2F2F2', image= close_img)
+Exit_Button.place(x=1700, y=50)
 
-
-lable_dict= tk.Label(window, text='T1')
-lable_dict.place(x=30,y=200)
+#----------- Entry Fields------
+#fileName_Entry = ctk.CTkEntry(master=frame_1, placeholder_text="CTkEntry")
+#-----------Check Boxes------
+check_Verdampfer = ctk.CTkCheckBox(master=window,text = "Verdampfer an")
+check_Verdampfer.place(x=100, y=50)
+check_Ventile = ctk.CTkCheckBox(master=window,text = "Ventile offen")
+check_Ventile.place(x=100, y=100)
 
 
 # Set Devices
@@ -266,9 +295,6 @@ patrone_8.start(-300)
 
 patronen_list = {'p1':patrone_1,'p_2':patrone_2,'p_3':patrone_3,'p_4':patrone_4,'p_5':patrone_5,'p_6':patrone_6,'p_7':patrone_7,'p_8':patrone_8}
 
-set_temp_dict = {}
-lable_dict = {}
-lable_t_ist_dict = {}
 option = ['T1','T2','T3','T4','T5','T6','T7','T8']
 
 frame_T_Reactor1 = tk.LabelFrame(window, text='T-Reactor 1')
@@ -277,7 +303,7 @@ frame_T_Reactor1.grid(column=0, row=1, padx=20, pady=20)
 frame_T_Reactor1.place(x=100,y= 200)
 
 alignment_var = tk.StringVar()
-
+""" 
 for num, i in enumerate(option):
     lable_dict[i]= tk.Label(frame_T_Reactor1, text=i)
     lable_dict[i].grid(column=0, row=num, ipadx=10, ipady=10)
@@ -289,7 +315,7 @@ for num, i in enumerate(option):
     lable_t_ist_dict[i].grid(column=3, row=num, ipadx=10, ipady=10)
 
 t_soll = {}
-
+ """
 
 MCFDual1 = TF_IndustrialDualAnalogIn(ipcon, "23UP")
 MFCDual2 = TF_IndustrialDualAnalogIn(ipcon, "23U6")
